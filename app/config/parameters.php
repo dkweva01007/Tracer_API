fa586ee76287ba0ae0f8ced4a22370d25a3e6495
Johann MARIE-REINE <johann.marie.reine@gmail.com>
2018-03-28 18:19:54 +0200

    up

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -1,8 +1,25 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$container->setParameter('database_host',      getenv('DATABASE_HOST')      ?: '127.0.0.1') ;
+$container->setParameter('database_port',      getenv('DATABASE_PORT')      ?: '3306') ;
+$container->setParameter('database_name',      getenv('DATABASE_NAME')      ?: 'dakota_booking') ;
+$container->setParameter('database_user',      getenv('DATABASE_USER')      ?: 'root') ;
+$container->setParameter('database_password',  getenv('DATABASE_PASSWORD')  ?: '') ;
 
+$container->setParameter('mailer_transport',  getenv('MAILER_TRANSPORT')  ?: 'smtp') ;
+$container->setParameter('mailer_host',       getenv('MAILER_HOST')       ?: 'ssl0.ovh.net') ;
+$container->setParameter('mailer_port',       getenv('MAILER_PORT')       ?: '587') ;
+$container->setParameter('mailer_user',       getenv('MAILER_USER')       ?: 'cap-adrenaline.com') ;
+$container->setParameter('mailer_encryption', getenv('MAILER_ENCRYPTION') ?: 'tls') ;
+$container->setParameter('mailer_password',   getenv('MAILER_PASSWORD')   ?: 'hjPi7CAGHOMp') ;
+$container->setParameter('mail_backup',       getenv('MAILER_BACKUP')     ?: 'Maildakotabox2@gmail.com') ;
+$container->setParameter('mailer_develloper', getenv('MAILER_DEVELOPPER') ?: 'johann.marie-reine@cap-adrenaline.com') ;
+$container->setParameter('mailer_debug',      '%mailer_develloper%') ;
+$container->setParameter('mailer_logs',       '%mailer_develloper%') ;
+
+$container->setParameter('locale',            getenv('locale')            ?: 'zn') ;
+
+
+$container->setParameter('secret',            getenv('SECRET')            ?: 'ThisTokenIsNotSoSecretChangeIt') ;
+
+?>
